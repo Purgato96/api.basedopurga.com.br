80d6c6478523e1e96b1403273345c6ff7a37b55d
Purgato96 <matheus_purgato@hotmail.com>
2025-10-20 17:11:50 -0300

    Implementando as permissões nos chats

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,16 +7,7 @@ use App\Models\Room;
 use App\Policies\RoomPolicy;
 
 class AppServiceProvider extends ServiceProvider {
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        Room::class => RoomPolicy::class,
-    ];
-
-    /**
+        /**
      * Register any application services.
      */
     public function register(): void {
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Room;
+use App\Policies\RoomPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as BaseAuthServiceProvider;
+
+class AuthServiceProvider extends BaseAuthServiceProvider {
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        Room::class => RoomPolicy::class,
+    ];
+
+    /**
+     * Register services.
+     */
+    public function register(): void {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void {
+        $this->registerPolicies();
+    }
+}
