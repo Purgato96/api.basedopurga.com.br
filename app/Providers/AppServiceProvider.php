c39a09cbfb8701914fdf8f438e2fad15d5d2e6e3
Purgato96 <matheus_purgato@hotmail.com>
2025-10-20 11:31:12 -0300

    Ajustando permissões e autorizações na aplicação

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,30 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Room;
+use App\Policies\RoomPolicy;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        Room::class => RoomPolicy::class,
+    ];
+
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
+    public function register(): void {
         //
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
+    public function boot(): void {
         //
     }
 }
