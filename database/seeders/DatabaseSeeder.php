6a4e2663f24635a3e63ffb594581c7eda7c4ba94
Purgato96 <matheus_purgato@hotmail.com>
2025-10-20 14:23:37 -0300

    Ajustando permissões e autorizações na aplicação

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder {
@@ -12,8 +12,29 @@ class DatabaseSeeder extends Seeder {
      * Seed the application's database.
      */
     public function run(): void {
+        // 1. Cria os Papéis e Permissões primeiro
         $this->call([
             RolesAndPermissionsSeeder::class,
         ]);
+
+        // 2. Busca o papel 'master' que acabou de ser criado
+        $masterRole = Role::findByName('master');
+
+        // 3. Cria seus usuários Master
+        User::firstOrCreate(
+            ['email' => 'matheuspurgato@gmail.com'],
+            [
+                'name' => 'Matheus Purgato', // Nome completo
+                'password' => Hash::make('Purgato$123**'),
+            ]
+        )->assignRole($masterRole); // Atribui o papel 'master'
+
+        User::firstOrCreate(
+            ['email' => 'm.purgato@interacti.com.br'],
+            [
+                'name' => 'Matheus Purgato InterACTI', // Nome
+                'password' => Hash::make('Purgato$123**'),
+            ]
+        )->assignRole($masterRole); // Atribui o papel 'master'
     }
 }
